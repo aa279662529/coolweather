95304f60f1593dae5948d32569076d4c45a9f086
Yi <yi@gmail.com>
2017-05-05 02:40:31 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/yi/com/coolweather/gson/AQI.java
@@ -0,0 +1,16 @@
+package yi.com.coolweather.gson;
+
+/**
+ * Created by Administrator on 2017/5/4.
+ */
+
+public class AQI {
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
---
+++ app/src/main/java/yi/com/coolweather/gson/Now.java
@@ -0,0 +1,21 @@
+package yi.com.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Administrator on 2017/5/4.
+ */
+
+public class Now {
+    @SerializedName("tmp")
+    public String temperature;
+
+    @SerializedName("cond")
+    public More more;
+
+    public class More{
+
+        @SerializedName("txt")
+        public String info;
+    }
+}
